fdaa2ad0ab52540fa72c7f7f9e6ef0f77fb14bb3
YuktiAgade <yuktiagade.cse24@sbjit.edu.in>
2026-03-31 21:12:36 +0530

    Create 7.1 Menu Driven Calculator (CALC).py

---
+++ 7.1 Menu Driven Calculator (CALC).py
@@ -0,0 +1,44 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+def mod(a, b):
+    return a % b
+
+while True:
+    print("\n--- CALCULATOR MENU ---")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Modulus")
+    print("6. Exit")
+
+    choice = int(input("Enter choice: "))
+
+    if choice == 6:
+        break
+
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+
+    if choice == 1:
+        print("Result:", add(a, b))
+    elif choice == 2:
+        print("Result:", sub(a, b))
+    elif choice == 3:
+        print("Result:", mul(a, b))
+    elif choice == 4:
+        print("Result:", div(a, b))
+    elif choice == 5:
+        print("Result:", mod(a, b))
+    else:
+        print("Invalid choice")
